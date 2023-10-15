8e54f442230bdaf3f7c11cf4ac8e85ade86b436d
Nicolas <nicolasdev.contact@gmail.com>
2023-10-15 17:46:24 -0300

    navbar resposive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent {}
+export class NavComponent {
+  isResponsive = false;
+
+  toggleNavigation() {
+    this.isResponsive = !this.isResponsive;
+  }
+}
